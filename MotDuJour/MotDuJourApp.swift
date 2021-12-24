f35ff3d3d56efa9516ce727adf205805fcb36859
Thibault Poujat <tpoujat@groupeseb.com>
2021-12-24 16:15:47 +0100

    Initial Commit

---
+++ MotDuJour/MotDuJourApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotDuJourApp.swift
+//  MotDuJour
+//
+//  Created by Thibault POUJAT on 24/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct MotDuJourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
