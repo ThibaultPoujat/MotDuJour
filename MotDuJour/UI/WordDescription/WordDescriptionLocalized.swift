c33be4548b5cfb94a14d580c6909b33fb6a56308
Thibault Poujat <thibault.poujat@gmail.com>
2021-12-27 15:41:46 +0100

    Add a localizable file that interface the native Localizable system

--- MotDuJour/UI/WordDescription/WordDescriptionLocalized.swift
+++ MotDuJour/UI/WordDescription/WordDescriptionLocalized.swift
@@ -1,8 +1,18 @@
 //
-//  WordLocalizable.swift
+//  WordDescriptionLocalized.swift
 //  MotDuJour
 //
 //  Created by Thibault POUJAT on 27/12/2021.
 //
 
 import Foundation
+
+/// Interface between native `Localizable` file and the `WordDescription` views.
+struct WordDescriptionLocalized {
+    
+    // Global
+    static let tabLabel = "word_description_tab_label".localized
+    
+    // WordDescription view
+    static let title = "word_description_header_title".localized
+}
