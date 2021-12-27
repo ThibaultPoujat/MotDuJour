c721d0d2fc209a7d8c2cd2f1b84da2f05596e4c3
Thibault Poujat <thibault.poujat@gmail.com>
2021-12-27 15:43:52 +0100

    Add a String extension that transform it to a localized version

--- MotDuJour/UI/Extension/String+Extension.swift
+++ MotDuJour/UI/Extension/String+Extension.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+/// Small extension to Localize Strings in this application.
+extension String {
+    
+    /// A localized version of this `String` if it exists in the `Localizable.strings` files.
+    var localized: String {
+        NSLocalizedString(self, bundle: .main, comment: "")
+    }
+    
+}
